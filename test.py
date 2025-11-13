51a49ca3f900717d35fb45914a908323e6b3e03c
kshireji <413457127@qq.com>
2025-11-13 09:03:55 +0800

    test

---
+++ test.py
@@ -0,0 +1,20 @@
+def give(b):
+    print("give执行")
+    return 4
+def main():
+    b=1
+    a=give(b)
+    print(a)
+
+
+high1 = []
+wide1 = []
+num1 = []
+num11 = []
+num12 = []
+
+
+
+
+if __name__ == '__main__':
+    main()
