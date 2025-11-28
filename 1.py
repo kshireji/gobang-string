c60563f69306588f8c2cab82d3d1739d5ad210b1
kshireji <413457127@qq.com>
2025-11-28 10:36:09 +0800

    new:2

--- 1.py
+++ 1.py
@@ -29,3 +29,9 @@ def printboard():
             print(Board[i][j], end=" ")
         print()
 
+def inputCheck(check):
+    incheck = input("请输入你的棋子,格式为x,y")
+    if incheck is not None:
+        x_check, y_check = incheck.split(sep=",")
+        if Board[int(x_check) - 1][int(y_check) - 1] == '-':
+            Board[int(x_check) - 1][int(y_check) - 1] = check
